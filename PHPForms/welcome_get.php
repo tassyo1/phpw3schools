34bd8a3245a3e339f4be465bff23401c146eafb7
Tássyo Santana <tsantana@novatecenergy.com.br>
2017-09-27 22:15:19 -0300

    PHP Forms - fim do form handling

---
+++ PHPForms/welcome_get.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+    Welcome <?php echo $_GET["name"]; ?>
+    Your email is <?php echo $_GET["email"];?>
+</body>
+</html>
+
+
+<!--
+    
+    $ _GET é uma matriz de variáveis passadas para o script atual através dos parâmetros de URL.
+    $ _POST é uma matriz de variáveis passadas para o script atual através do método HTTP POST. -->
